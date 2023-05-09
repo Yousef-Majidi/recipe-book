cfff8561e07231d170167fedadedb286b355c8e4
Yousef <y.majidin@gmail.com>
2023-05-09 13:19:21 -0400

    implemented shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root', // shopping list service will be used by the recipes module as well, so we need to provide it in the root module
 })
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
 
-  constructor() { }
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor() {}
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice()); // emit a copy of the ingredients array
+  }
 }
